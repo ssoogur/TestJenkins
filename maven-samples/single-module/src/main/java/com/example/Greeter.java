11f66cb93a1e76eaf3135d024c5fb0d37d4b6a2d
ssoogur <55739704+ssoogur@users.noreply.github.com>
2021-11-22 21:05:54 -0500

    this is java Main change corrected Compilation

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -16,7 +16,7 @@ public class Greeter {
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
-  public static void main(String args){
+  public static void main(String args[]){
     System.out.println("Hello This is comming from Main method from Greeter");
   }
 }
